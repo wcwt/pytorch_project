72a991d0482cd1dbdc0642c74a90e409c63355ba
wcwt <51042202+wcwt@users.noreply.github.com>
2020-09-29 23:27:40 +0800

    til 7:30 https://www.youtube.com/watch?v=i2yPxY2rOzs

--- digit_recongize/main.py
+++ digit_recongize/main.py
@@ -1,1 +1,12 @@
 import torch
+import torchvision
+from torchvision import transforms,datasets
+
+train = datasets.MNIST("",train=True,download=True,
+                    transforms = transforms.Compose([transforms.ToTensor()]))
+
+trainset = torch.utils.data.DataLoader(train,batch_size=10,shuffle=True)
+testset  = torch.utils.data.DataLoader(test,batch_size=10,shuffle=True)
+
+for data in trainset:
+    print(data)
