0484e10b7ecb0b39d809aeed95dc8a058af38a65
wcwt <51042202+wcwt@users.noreply.github.com>
2020-09-30 16:19:26 +0800

    downloaded test sample

--- digit_recongize/main.py
+++ digit_recongize/main.py
@@ -3,7 +3,8 @@ import torchvision
 from torchvision import transforms,datasets
 
 train = datasets.MNIST("",train=True,download=True,
-                    transforms = transforms.Compose([transforms.ToTensor()]))
+                    #transforms = transforms.Compose([transforms.ToTensor()])
+                    )
 
 trainset = torch.utils.data.DataLoader(train,batch_size=10,shuffle=True)
 testset  = torch.utils.data.DataLoader(test,batch_size=10,shuffle=True)
