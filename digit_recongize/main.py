bc9377d3b5e15460b6ced4eddb950a10e69d1f65
wcwt <51042202+wcwt@users.noreply.github.com>
2020-09-30 22:31:18 +0800

    train_one

--- digit_recongize/main.py
+++ digit_recongize/main.py
@@ -16,9 +16,23 @@ test = train = datasets.MNIST("",train=False, download=False,
 # lesses batches ==>less sample at a time > more optimizes times will do and the more general rule will be generated !!!
 trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
 testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=True)
+pixels = 28*28
+ls = [pixels,64,64,64,10] # layer_size
+num_of_layer = len(ls) - 1
 
 class Net(nn.Module):
-
     def __init__(self):
-        super().__init__()
-hello
+        self.fc = [] # fully connected layer
+        super().__init__() # the same as nn.Module.__init__()
+        for i in range(num_of_layer):
+            self.fc.append( nn.Linear(ls[i], ls[i+1]) ) # input,output # get the seeting of the layer
+
+    def forward(self,x):
+        # set activation function in the middle layer
+        for i in range(num_of_layer - 1 ):
+            x = F.relu( self.fc[i](x) )
+        x = self.fc[-1](x) # the last layer no need any activation function
+        return F.log_softmax(x, dim=1)
+
+net = Net()
+print(net)
--- digit_recongize/test.py
+++ digit_recongize/test.py
@@ -1,11 +1,14 @@
-class Animals():
+class Animals:
     def welcome(self):
         print("Hello  am Tom")
 
 class dog(Animals):
     def welcome(self):
-        super.welcome()
+        super().welcome()
+        print("I am Dog")
 
+for i in range(4):
+    print(i)
 
-a = dog()
-a.dog()
+d = dog()
+d.welcome()
